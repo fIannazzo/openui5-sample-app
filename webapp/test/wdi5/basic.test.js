989a23e1e776304d47f33823c876b29ecec65b40
Francesco Iannazzo <francesco.iannazzo@devroyal.eu>
2022-09-14 10:02:56 +0200

    WDI5 Setup and Simple test

---
+++ webapp/test/wdi5/basic.test.js
@@ -0,0 +1,20 @@
+const _ui5Service = require("wdio-ui5-service").default;
+const wdioUI5Service = new _ui5Service(); 
+// await wdioUI5Service.injectUI5();
+
+describe("My Login application", () => {
+	it("before", async () => {
+		await browser.url(`http://localhost:8080/index.html`);
+		await $("div[id=container-todo---app]").waitForExist();
+		await wdioUI5Service.injectUI5();
+		const selector = {
+			forceSelect: true,
+			selector: {
+				id: "container-todo---app--todoList",
+			},
+		};
+		const oList = await browser.asControl(selector);
+		console.log("List ID " + (await oList.getId()));
+        console.log("Table ID " + (await oList.getItems()));
+	});
+});
