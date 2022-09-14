ebb58d282d940fc667f5863a9dfc70ec238ce851
Francesco Iannazzo <francesco.iannazzo@devroyal.eu>
2022-09-14 15:47:01 +0200

    Add basic latetest

--- webapp/test/wdi5/basiclate.test.js
+++ webapp/test/wdi5/basiclate.test.js
@@ -1,5 +1,11 @@
+const _ui5Service = require("wdio-ui5-service").default;
+const wdioUI5Service = new _ui5Service(); 
+
 describe("My Login application", () => {
 	it("before", async () => {
+		//await browser.url(`http://localhost:8080/index.html`);
+		//await $("div[id=container-todo---app]").waitForExist();
+		await wdioUI5Service.injectUI5();
 		const selector = {
 			forceSelect: true,
 			selector: {
@@ -10,5 +16,6 @@ describe("My Login application", () => {
 		const oList = await browser.asControl(selector);
 		console.log("List ID " + (await oList.getId()));
         expect((await oList.getItems()).length).toEqual(2);
+        //console.log("Table ID " + (await oList.getItems()));
 	});
 });
