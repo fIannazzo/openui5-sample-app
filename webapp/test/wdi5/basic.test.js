19ecc4c799518b3abe659d9ded1930ee5e2b749b
Francesco Iannazzo <francesco.iannazzo@devroyal.eu>
2022-09-14 15:32:15 +0200

    Remove late injection

--- webapp/test/wdi5/basic.test.js
+++ webapp/test/wdi5/basic.test.js
@@ -1,20 +1,22 @@
 const _ui5Service = require("wdio-ui5-service").default;
-const wdioUI5Service = new _ui5Service(); 
+// const wdioUI5Service = new _ui5Service(); 
 // await wdioUI5Service.injectUI5();
 
 describe("My Login application", () => {
 	it("before", async () => {
-		await browser.url(`http://localhost:8080/index.html`);
-		await $("div[id=container-todo---app]").waitForExist();
-		await wdioUI5Service.injectUI5();
+		//await browser.url(`http://localhost:8080/index.html`);
+		//await $("div[id=container-todo---app]").waitForExist();
+		//await wdioUI5Service.injectUI5();
 		const selector = {
 			forceSelect: true,
 			selector: {
 				id: "container-todo---app--todoList",
+                interaction: "root"
 			},
 		};
 		const oList = await browser.asControl(selector);
 		console.log("List ID " + (await oList.getId()));
-        console.log("Table ID " + (await oList.getItems()));
+        expect((await oList.getItems()).length).toEqual(2);
+        //console.log("Table ID " + (await oList.getItems()));
 	});
 });
